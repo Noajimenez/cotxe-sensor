e5d91e759ae7dd0ec0bd3ee934924658e0fbbd13
Noajimenez <82875952+Noajimenez@users.noreply.github.com>
2021-05-28 14:38:03 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,23 @@
+let costat = 0
 basic.forever(function () {
-    if (cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters) <= 10) {
-        cuteBot.moveTime(cuteBot.Direction.backward, 70, randint(0.1, 0.5))
-    } else if (cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters) < 2) {
-        cuteBot.moveTime(cuteBot.Direction.backward, 50, randint(0.7, 1.3))
+    if (cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters) > 1 && cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters) < 30) {
+        costat = randint(1, 2)
+        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff0000)
+        if (costat == 1) {
+            cuteBot.moveTime(cuteBot.Direction.right, -50, randint(0.1, 0.3))
+        } else {
+            cuteBot.moveTime(cuteBot.Direction.left, -50, randint(0.1, 0.3))
+        }
     } else {
-        cuteBot.motors(50, 50)
+        cuteBot.motors(100, 100)
     }
+})
+basic.forever(function () {
+    basic.showLeds(`
+        # . . . #
+        # # . . #
+        # . # . #
+        # . . # #
+        # . . . #
+        `)
 })
